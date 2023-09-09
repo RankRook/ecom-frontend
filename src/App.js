bf18d03b1ed3a0655c010acd9f06d8969030e76b
Rankrook <rankrook@gmail.com>
2023-09-09 17:07:47 +0700

    single product

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Login from "./pages/Login";
 import Forgotpassword from "./pages/Forgotpassword";
 import Signup from "./pages/Signup"
 import SingleBlog from "./pages/SingleBlog";
+import SingleProduct from "./pages/SingleProduct";
 function App() {
   return (
     <BrowserRouter>
@@ -21,7 +22,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="store" element={<OurStore />} />
+          <Route path="product" element={<OurStore />} />
+          <Route path="product/:id" element={<SingleProduct />} />
           <Route path="blogs" element={<Blog />} />
           <Route path="blog/:id" element={<SingleBlog />} />
           <Route path="compareproduct" element={<CompareProduct />} />
@@ -29,6 +31,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="forgot-password" element={<Forgotpassword />} />
           <Route path="signup" element={<Signup />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,9 +9,9 @@ const ProductCard = (props) => {
   return (
     <>
       <div
-        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
+        className={`${location.pathname === "/product" ? `gr-${grid}` : "col-3"}`}
       >
-        <Link className="product-card position-relative">
+        <Link to="/product/:id" className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
             <Link>
               <img src="images/wish.svg" alt="wishlist" />
@@ -60,7 +60,7 @@ const ProductCard = (props) => {
         </Link>
       </div>
       <div
-        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
+        className={`${location.pathname === "/product" ? `gr-${grid}` : "col-3"}`}
       >
         <Link className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
